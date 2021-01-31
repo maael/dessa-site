f60fcb8848b5798441754c0dd97151ec8e82d12d
Matthew Elphick <matt.a.elphy@gmail.com>
2021-01-31 15:52:39 +0000

    fix: Make header more mobile friendly

--- src/components/primitives/HeaderNav.tsx
+++ src/components/primitives/HeaderNav.tsx
@@ -2,12 +2,12 @@ import Spinner from './Spinner'
 
 export default function HeaderNav() {
   return (
-    <div className="pl-20 pr-20 flex items-center justify-center">
-      <div className="w-20 -ml-20">
+    <div className="pl-20 pr-20 md:-mb-10 flex items-center justify-center">
+      <div className="w-20 -ml-20 hidden md:block">
         <Spinner />
       </div>
-      <div className="z-50 flex items-center -mt-5">
-        <div className="title text-3xl pl-2 pr-5">
+      <div className="z-50 flex items-center md:-mt-5">
+        <div className="title text-3xl pl-2 pr-5 hidden md:block">
           <a href="/">Dessa</a>
         </div>
         <div className="title text-1xl pr-4">
