11a4c5610d2168146cfddc756021b0e5bbc720ef
Matthew Elphick <matthew.elphick@threadsstyling.com>
2021-02-03 08:43:30 +0000

    feat: 🎸 Initial supabase work

--- src/pages/sightseeing/index.tsx
+++ src/pages/sightseeing/index.tsx
@@ -1,8 +1,9 @@
 import Link from 'next/link'
 import HeaderNav from '../../components/primitives/HeaderNav'
-import { SightseeingList, SightseeingEntry } from '../../types/props'
+import useSupa from '../../components/hooks/useSupa'
 
-export default function Sightseeing({ entries }: { entries: (SightseeingEntry & { id: string })[] }) {
+export default function Sightseeing() {
+  const { data } = useSupa<{ id: number; name: string }>('sightseeing_chapters', 'id, name')
   return (
     <div>
       <HeaderNav />
@@ -13,7 +14,7 @@ export default function Sightseeing({ entries }: { entries: (SightseeingEntry & 
         </Link>
       </div>
       <div className="flex flex-col justify-center items-center mt-5 w-full pr-2 pl-2">
-        {entries.map((i) => (
+        {data.map((i) => (
           <Link key={i.id} href={`/sightseeing/${i.id}`}>
             <a className="w-full mb-5 grid gap-10 md:gap-20 grid-cols-1 md:grid-cols-5 xl:grid-cols-7 place-content-center mr-2 ml-2">
               <div className="md:col-start-2 xl:col-start-3 col-span-3 flex flex-col justify-center shadow-lg rounded-md bg-blue-900 text-white pr-4 pl-4 pt-6 pb-6">
@@ -25,12 +26,4 @@ export default function Sightseeing({ entries }: { entries: (SightseeingEntry & 
       </div>
     </div>
   )
-}
-
-export async function getStaticProps() {
-  const sightseeingInfo: SightseeingList = require('../../../data/sightseeing.json')
-
-  return {
-    props: { entries: Object.entries<any>(sightseeingInfo).map(([id, { locations, ...entry }]) => ({ id, ...entry })) },
-  }
 }
