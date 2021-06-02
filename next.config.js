513ed3ea111ff830d43c7513eaf4762c239b6e8c
Matthew Elphick <matt.a.elphy@gmail.com>
2021-06-02 18:02:33 +0100

    fix: Handle other aws s3 upload urls for next image

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 // eslint-disable-next-line no-undef
 module.exports = {
   images: {
-    domains: ['render.guildwars2.com', 'static.staticwars.com', 'dessa-production.s3.eu-west-2.amazonaws.com'],
+    domains: [
+      'render.guildwars2.com',
+      'static.staticwars.com',
+      'dessa-production.s3.amazonaws.com',
+      'dessa-production.s3.eu-west-2.amazonaws.com',
+    ],
   },
 }
